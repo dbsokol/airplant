71358332a17ba40ba8b5995384018d5a2c1c5edb
dbsokol <danielbsokol@gmail.com>
2019-12-11 21:02:42 +0000

    iAdded in tools

---
+++ Tools/Tools.py
@@ -0,0 +1,9 @@
+
+
+def PrintTitle(function_name):
+    
+    border = '   [' + '#' * (len(function_name) + 10) + ']\n'
+    middle = '   [##| [' + function_name + '] |##]\n'  
+    
+    print('\n' + border + middle + border)
+    
