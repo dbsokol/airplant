a84b4ba2430b2dba978db8f6899ae93911684357
dbsokol <danielbsokol@gmail.com>
2019-12-16 20:18:40 +0000

    added in static-root

--- airplant/settings.py
+++ airplant/settings.py
@@ -131,10 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/airplant/static/',
-]
+#STATICFILES_DIRS = [
+#    '/airplant/static/',
+#]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
